5a13dbd093957fc56c91104928c2cc15835b339e
EricX003 <90646214+EricX003@users.noreply.github.com>
2023-03-13 09:36:48 -0500

    Update detect.py
    Signed-off-by: EricX003 <90646214+EricX003@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -33,6 +33,7 @@ import os
 import platform
 import sys
 from pathlib import Path
+from google.colab.patches import cv2_imshow
 
 import torch
 
@@ -181,7 +182,7 @@ def run(
                     windows.append(p)
                     cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
                     cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
+                cv2_imshow(im0)
                 cv2.waitKey(1)  # 1 millisecond
 
             # Save results (image with detections)
